2dbcbde868581dfc47448bce95f07aedae5e12b4
devikachhabra78 <devika1467.becse24@chitkara.edu.in>
2025-01-25 20:24:09 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -2,40 +2,40 @@
 int main(){
     int a;
     scanf("%d",&a);
-    if(a == '1'){
+    if(a == 1){
         printf("January");
     }
-    else if(a == '2'){
+    else if(a == 2){
         printf("February");
     }
-    else if(a == '3'){
+    else if(a == 3){
         printf("March");
     }
-    else if(a == '4'){
+    else if(a == 4){
         printf("April");
     }
-    else if(a == '5'){
+    else if(a == 5){
         printf("May");
     }
-    else if(a == '6'){
+    else if(a == 6){
         printf("June");
     }
-    else if(a == '7'){
+    else if(a == 7){
         printf("July");
     }
-    else if(a == '8'){
+    else if(a == 8){
         printf("August");
     }
-    else if(a == '9'){
+    else if(a == 9){
         printf("September");
     }
-    else if(a == '10'){
+    else if(a == 10){
         printf("October");
     }
-    else if(a == '11'){
+    else if(a == 11){
         printf("November");
     }
-    else if(a == '12'){
+    else if(a == 12){
         printf("December");
     }
     else{
